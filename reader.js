50def212da3f2933274d7bb02e84c2d835d4721a
isaaclopinho <isaaclopinho@gmail.com>
2020-08-28 14:23:18 -0300

    logs ands improvements

--- reader.js
+++ reader.js
@@ -12,6 +12,7 @@ let dataFromXLSX = [];
 
 // Reading the files credoresAptosVotacaoAGC/[0-2].xlsx removing the lines that contains unnecessary information
 for (let i = 0; i < 3; i++) {
+  console.log("Reading " + `${__dirname}/credoresAptosVotacaoAGC/`+ i +`.xlsx` + " file.");
   let workSheetsFromBuffer = xlsx.parse(fs.readFileSync(`${__dirname}/credoresAptosVotacaoAGC/`+ i +`.xlsx`));
 
   workSheetsFromBuffer = workSheetsFromBuffer[0].data.filter((x) => {
@@ -27,71 +28,73 @@ for (let i = 0; i < 3; i++) {
 
 console.log("Number of lines: " + dataFromXLSX.length);
 
-let dataFiltered = dataFromXLSX.map( x=> {
-  let nome, classe, reais, euros, libras, usd;
+let dataFiltered = dataFromXLSX.map((x) => {
+  let nome, classe, BRL, EUR, POUNDS, USD;
+  let total = 0;
 
   classe = x[1];
   nome = x[2];
-  cpf = x[3].slice(0, -1)
+  cpf = x[3].slice(0, -1);
 
-
-let total = 0;
-
-  if(x.includes("R$")){
+  if (x.includes("R$")) {
     let index = x.indexOf("R$");
-    
-    for(let i=1; i < 4; i++){
-      if(x[index+i] !== undefined){
-        reais = x[index+i];
+
+    for (let i = 1; i < 4; i++) {
+      if (x[index + i] !== undefined) {
+        BRL = x[index + i];
         break;
       }
     }
   }
 
-  if(x.includes("USD")){
+  if (x.includes("USD")) {
     let index = x.indexOf("USD");
-    
-    for(let i=1; i < 5; i++){
-      if(x[index+i] !== undefined){
-        usd = x[index+i];
+
+    for (let i = 1; i < 5; i++) {
+      if (x[index + i] !== undefined) {
+        USD = x[index + i];
         break;
       }
     }
-  
   }
 
-  if(x.includes("€")){
+  if (x.includes("€")) {
     let index = x.indexOf("€");
-    
-    for(let i=1; i < 4; i++){
-      if(x[index+i] !== undefined){
-        euros = x[index+i];
+
+    for (let i = 1; i < 4; i++) {
+      if (x[index + i] !== undefined) {
+        EUR = x[index + i];
         break;
       }
     }
   }
 
-  if(x.includes("£")){
+  if (x.includes("£")) {
     let index = x.indexOf("£");
-    
-    for(let i=1; i < 3; i++){
-      if(x[index+i] !== undefined){
-        libras = x[index+i];
+
+    for (let i = 1; i < 3; i++) {
+      if (x[index + i] !== undefined) {
+        POUNDS = x[index + i];
         break;
       }
     }
   }
 
-  total += reais === undefined ? 0 : reais;
-  total += usd === undefined ? 0 : (usd * config["USD"]);
-  total += euros === undefined ? 0 : (euros * config["EURO"]);
-  total += libras === undefined ? 0 : (libras * config["POUND"]);
+  total += BRL === undefined ? 0 : BRL;
+  total += USD === undefined ? 0 : USD * config["USD"];
+  total += EUR === undefined ? 0 : EUR * config["EURO"];
+  total += POUNDS === undefined ? 0 : POUNDS * config["POUND"];
 
-  return [classe, cpf, nome, reais, usd, euros, libras, total];
+  return [classe, cpf, nome, BRL, USD, EUR, POUNDS, total];
 });
 
+// CSV Header
+let CSVHeader =  [["classe", "cpf", "nome", "reais", "usd", "euros", "libras", "total"]]
 // Adding header to dataFiltered
-let CSVData = [["classe", "cpf", "nome", "reais", "usd", "euros", "libras", "total"]].concat(dataFiltered);
+let CSVData = CSVHeader.concat(dataFiltered);
+
+
+console.log("Creating CSV file: " + config.filePath);
 
 //Creating the csv file
 var file = fs.createWriteStream(config.filePath, {flags: 'w' });
@@ -99,11 +102,14 @@ var file = fs.createWriteStream(config.filePath, {flags: 'w' });
 CSVData.forEach(element => {
     let str = '';
     for(let i=0;i < element.length; i++){
-      let el = element[i] === undefined ? '0' : element[i];
-        str+= (el);
-        str += (i==element.length-1? '\n' : '\t')
+        str += element[i] === undefined ? '0' : element[i];;
+        str += (i==element.length-1 ? '\n' : '\t')
     }
     file.write(str);
 });
 
 file.end();
+
+console.log("CSV file has been created");
+console.log("CSV separator = TAB")
+console.log("CSV header = " + CSVHeader)
